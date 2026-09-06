f2ad7db8b2380b2b7d62777e4be8333c57e4ba90
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-06 09:43:49 +0100

    Create learncpp.cpp

---
+++ fundamentals/learncpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    int age = 25;
+    double price = 19.99;
+    char grade = 'A';
+    bool isActive = true;
+    std::string name = "John";
+
+    return 0;
+}
