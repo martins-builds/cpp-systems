762361ab43409a7229fb06f7da1a9e196e607630
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-08 10:09:59 +0100

    Update references.cpp

--- fundamentals/references.cpp
+++ fundamentals/references.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+void work(int x){
+
+}
 int main(){
     int a = 5;
+    int c = 10;
     int &b = a;
 
     std :: cout << b+5 << std :: endl;
+    work(c);
     return 0;
 }
