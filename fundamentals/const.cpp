51733fc25d7a8828d71a6c130acd0d3c91cd7612
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-08 11:21:08 +0100

    class

--- fundamentals/const.cpp
+++ fundamentals/const.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <string>
 
+class Entity{
+    private:
+        int m_X, m_Y;
+    public:
+        int GetX() const{
+            return m_X;
+        }
+        void SeetX(int x){
+            m_X = x;
+        }
+};
+
 int main(){
     const int MAX_AGE = 90;
     int *a = new int;
     *a = 2;
 
     std::cout << *a << std::endl;
+    std::cin.get();
     return 0;
 }
