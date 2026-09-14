d1b768ed9c39866d252b10341f6dbb8b27232ee9
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-14 11:43:14 +0100

    Update conditional.cpp

--- fundamentals/conditional.cpp
+++ fundamentals/conditional.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
+using namespace std;
+    struct sct {
+    int t[2];
+};
+    struct str {
+    sct t[2];
+};
 
 int main(void){
+    str t[2] = { {0, 2, 4, 6}, {1, 3, 5, 7} };
     int c0;
     int steps = 0;
-    std::cin >> c0;
+    cin >> c0;
     while (c0 != 1){
         if (c0 % 2 == 0)
             c0 /= 2;
         else
             c0 = (3 * c0) + 1;
-        std::cout << c0 << std::endl;
+        cout << c0 << endl;
         steps++;
     }
-    std::cout << steps << std::endl;
+    cout << steps << endl;
+
+    cout << t[1].t[0].t[1] << t[0].t[1].t[0];
     return 0;
 }
