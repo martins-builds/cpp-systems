2a883c7921df3dc46b15cbee59ede370ed640330
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-14 10:38:45 +0100

    Update conditional.cpp

--- fundamentals/conditional.cpp
+++ fundamentals/conditional.cpp
@@ -2,12 +2,14 @@
 
 int main(void){
     int c0;
+    int steps = 0;
     std::cin >> c0;
     while (c0 != 1){
         if (c0 % 2 == 0)
             c0 /= 2;
         else
             c0 = (3 * c0) + 1;
+        std::cout << c0 << std::endl;
     }
     
     return 0;
