ccf51d2a6adbfc6dfa1135bad9dd82c87546667f
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-22 12:58:26 +0100

    testing types size in cpp

--- fundamentals/pointers.cpp
+++ fundamentals/pointers.cpp
@@ -5,5 +5,16 @@ int main(){
     int i, *p;
     i = 2;
     p = &i;
+    cout << *p << endl;
+    cout << "This computing environment uses:" << endl;
+    cout << sizeof(char) << " bytes for chars" << endl;
+    cout << sizeof(short int) << " bytes for shorts" << endl;
+    cout << sizeof(int) << " bytes for ints" << endl;
+    cout << sizeof(long int) << " bytes for longs" << endl;
+    cout << sizeof(float) << " bytes for floats" << endl;
+    cout << sizeof(double) << " bytes for doubles" << endl;
+    cout << sizeof(bool) << " byte for bools" << endl;
+    cout << sizeof(int * ) << " bytes for pointers" << endl;
+    
     return 0;
 }
